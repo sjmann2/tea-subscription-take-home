79103808b3f062b175ef065b31a4825f47ff0e8b
Sid Mann <sjmann2@outlook.com>
2023-01-10 16:41:15 -0600

    Update model and factory for new price attribute

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
    description { Faker::Tea.type }
    temperature { [175, 180, 195, 212].sample }
    brew_time {(3..7).to_a.sample}
+   price {{ Faker::Number.decimal(l_digits: 2) }}
    end
  end
